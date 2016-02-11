159ac80968c5d9a98ea4dba2f5b61451a5a2e3a0
Mfioravan <mfioravan@gmail.com>
2016-02-11 13:29:39 -0500

    Added make file, OpenGL

--- Zombie Tower Defense/Main.cpp
+++ Zombie Tower Defense/Main.cpp
@@ -7,3 +7,52 @@
 //
 
 #include "Main.hpp"
+#ifdef OSX
+    #include <GLUT/glut.h>
+#else
+    #include <GL/glew.h>
+    #include <GL/glut.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function Prototypes
+void display();
+
+int main(int argc, char *argv[]){
+    // Initialize GLUT
+    glutInit(&argc, argv);
+    
+    // Initialize the window with double buffering and RGB colors
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    
+    // Set window size
+    glutInitWindowSize(512, 512);
+    
+    // Create WIndow
+    glutCreateWindow("Tower Defense");
+    
+#ifndef OSX
+    // Initialize GLEW
+    glewInit();
+#endif
+    
+    // Define Callbacks
+    glutDisplayFunc(display);
+    
+    // Set Background Color
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    
+    // Begin event loop
+    glutMainLoop();
+    return 0;
+}
+
+// Display Callback
+void display(){
+    // Flush Buffer
+    glFlush();
+    
+    // Swap Buffers
+    glutSwapBuffers();
+}
